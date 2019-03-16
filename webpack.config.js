07ff3e7efb34633d9a5613d5c4957588a3394be1
Alex Milovanov <alex.milovanov.mb@gmail.com>
2019-03-16 16:48:43 +0300

    add simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const html_webpack_plugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'build.js',
+        path: path.join(__dirname, '/dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '/dist')
+    },
+    module: {
+        rules: [{
+            test: /\.(jsx|js|tsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new html_webpack_plugin({
+            template: path.join(__dirname, '/src/index.html')
+        })
+    ]
+};
