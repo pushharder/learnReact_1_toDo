f2ec4ae3c8d3c34bc3c9d9aced4135c1fd7c2ced
Alexander Milovanov <alexander_milovanov@epam.com>
2019-03-17 12:33:28 +0300

    add styled component to footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import {FooterWrapper} from './FooterStyles';
 
 export class Footer extends React.Component {
     render(){
         return (
-            <div>
+            <FooterWrapper>
                 footer
-            </div>
+            </FooterWrapper>
         )
     }
 }
